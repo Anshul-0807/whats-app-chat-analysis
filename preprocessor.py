ceaaecf0eb0937d64cd4e94506356e7526b86eef
Anshul patle <111644282+Anshul-0807@users.noreply.github.com>
2023-06-25 23:41:23 +0530

    Add files via upload

--- preprocessor.py
+++ preprocessor.py
@@ -1,1 +1,8 @@
-def preprocessor(data):
+import re
+import pandas as pd
+
+def preprocess(data):
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+
+    messages = re.split(pattern, data)
+    dates = re.findall(pattern, data)
