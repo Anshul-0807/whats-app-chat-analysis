7660c4fb66486ac4756708be0cc5e8a0dea42ef9
Anshul patle <111644282+Anshul-0807@users.noreply.github.com>
2023-06-26 23:41:43 +0530

    Add files via upload

--- preprocessor.py
+++ preprocessor.py
@@ -6,3 +6,20 @@ def preprocess(data):
 
     messages = re.split(pattern, data)
     dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    # convert message_date type
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:  # user name
+            users.append(entry[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
