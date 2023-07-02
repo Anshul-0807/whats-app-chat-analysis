3f622481854d4531b5edf514aa26df88be8bf272
Anshul patle <111644282+Anshul-0807@users.noreply.github.com>
2023-07-02 18:15:23 +0530

    Add files via upload

--- helper.py
+++ helper.py
@@ -1,18 +1,29 @@
+from urlextract import URLExtract
+extract = URLExtract()
 def fetch_stats(selected_user,df):
 
-    if selected_user == 'Overall':
-        # 1. fetch number of messages
-        num_messages = df.shape[0]
-        # number of words
-        words = []
-        for message in df['message']:
-            words.extend(message.split())
-        return num_messages, len(words)
-    else:
-        new_df = df[df['user'] == selected_user]
-        num_messages = new_df.shape[0]
-        words = []
-        for message in new_df['message']:
-            words.extend(message.split())
-        return num_messages, len(words)
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # fetch the number of messages
+    num_messages = df.shape[0]
+
+    # fetch the number of words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    # fetch the no. of media messages
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    #  FETCH NO. OF LINK SHARED
+
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+
+    return num_messages, len(words), num_media_messages, len(links)
+
+
 
